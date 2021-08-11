cec8227eae017fad417832e51cc9d488761ebe6c
Eron <79117498+CzarnyCsCode@users.noreply.github.com>
2021-08-11 23:18:17 +0200

    Update index.php

--- index.php
+++ index.php
@@ -629,31 +629,7 @@
 
   <!--========= JS FB ==========-->
 
-  <div id="fb-root"></div>
-      <script>
-        window.fbAsyncInit = function() {
-          FB.init({
-            xfbml            : true,
-            version          : 'v10.0'
-          });
-        };
-
-        (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/pl_PL/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));</script>
-
-      <!-- CHAT PL -->
-      <div class="fb-customerchat"
-        attribution="setup_tool"
-        page_id="100591262050431"
-  theme_color="#bf00fc"
-  logged_in_greeting="Cześć! W czym mogę ci pomóc?"
-  logged_out_greeting="Cześć! W czym mogę ci pomóc?">
-      </div>
+ //
 
   <!--========= END JS FB ==========-->
 
